01446140e91396f255ab6dbcc1e89a92e00daf1f
gosia_straszewska <malgorzata.straszewska@gmail.com>
2022-04-02 12:16:58 +0200

    stack example

---
+++ src/main/java/app/CheckBrackets.java
@@ -0,0 +1,30 @@
+package app;
+
+import stack.StringStack;
+
+public class CheckBrackets {
+    public static boolean checkBrackets(String s){
+        StringStack stack = new StringStack();
+        for (int i = 0; i < s.length(); i++) {
+            String letter = s.charAt(i) + "";
+            switch (letter){
+                case "(":
+                    stack.push("(");
+                    break;
+                case ")":
+                    if(stack.isEmpty()){
+                        return false;
+                    }
+                    stack.pop();
+                    break;
+                default:
+
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String str = "(A+B)-458(V-(a/6)/6+7)";
+    }
+}
